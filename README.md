77b56569efdf5c3f29933e45b355c95c5bff835f
Dikaeinstein <solozyokwa@gmail.com>
2017-08-09 04:53:40 +0100

    Major improvements. Please read change log for updates.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Calculator
+# calculator
+
 A basic calculator written with JavaScript
 
+## Features
+
+- supports numbers of length upto 16-digits.
+- Handles 'Big numbers'(floats and ints) well.
+- Performs basic math operations.
+
+This project have been made possible thanks to [big.js](https://github.com/MikeMcl/big.js/).
+
+## Feedback
+
+Feedback is welcome.
+
+Bugs/comments/questions?
+Open an issue, or email
+
+Solomon
+<a href="mailto:solozyokwa@gmail.com">solozyokwa@gmail.com</a>
+
+## License
+
+See License.
+
+## change Log
+
+#### 1.1
+
+* Added [big.js](https://github.com/MikeMcl/big.js/) library.
+* Added support for 'Big numbers'(floats and ints) i.e better handling of JS IEEE-756 Number type.
+* Improved interface by using standard calculator operator symbols.
+* Improved performance and overall behaviour of math operations.
+
+#### 1.0.0
+
+* 02/08/2017 Initial release
--- public/js/main.js
+++ public/js/main.js
@@ -1,105 +1,141 @@
 /* jshint node: true, esversion: 6 */
-const operators = {
-    "+": function sum(x, y) {
-        return new Big(x).plus(y);
-    },
-    "-": function sub(x, y) {
-        return new Big(x).minus(y);
-    },
-    "x": function mul(x, y) { 
-        return new Big(x).times(y).round(8);
-    },
-    "/": function div(x, y) {
-        return new Big(x).div(y).round(8);
-    }
-};
-let handleTotal = (bigTotal) => (bigTotal.e > 12) ? bigTotal.toExponential() : bigTotal.toString();
-
-function clickNumber(e) {
-    var mainDisp = $("#main");
-    var value = $(e.target).val();
-    var numElement = `<div class="main-number">${value}</div>`;
-    
-    // handle number length in the main display
-    if (mainDisp.contents().length >= 12) {
-        // if number length exceeds 12 scale down font-size to
-        // to accumulate upto 16 numbers
-        if (mainDisp.contents().length < 16) {
-            mainDisp.css("font-size", "4.2rem");
-            mainDisp.append(numElement);
-        }
-        else
-            return false;
-    }
-    else {
-        mainDisp.css("font-size", "5.4rem");
-        // display numbers
-        if ($("#main div:first").text() === "0" && $("#main div").length === 1 && value === "0")
-            return false;
-        else if ($("#main div:first").text() === "0" && value !== "." && $("#main div").length === 1)
-            mainDisp.html(numElement); 
-        else if ($("#main div:first").text() === "0" && value === ".")
-            mainDisp.append(numElement);
-        else if ($("#main div:first").text() === "0" && $($("#main div").get(1)).text() === ".")
-            mainDisp.append(numElement);
-        else if ($("#main div").is(".main-result")) 
-            mainDisp.html(numElement);
-        else
-            mainDisp.append(numElement);
-    }    
-}
-function clickClear(e) {
-    $("#operation").empty();
-    $("#main").html("<div class='main-number'>0</div>");
-    return false;
-}
-function clickBs(e) {
-    var last = $("#main div:last");
-    if ($("#main div").length > 1)
-        last.remove();
-    else  if ($("#main div").length === 1)
-        $("#main").html("<div class='main-number'>0</div>");
-    else
-        return false;
-}
-function clickOperator(e) {
-    var operation = $("#operation");
-    var operator, number, operationElem;
-    if (!$("#main div").length)
-        return false;
-    else {
-        operator = $(e.target).val();
-        number = $("#main").text();
-        operationElem = `<div class="operation-num"> ${number} </div><div class="operation-op">${operator}</div>`;
-        operation.append(operationElem);
-        $("#main").empty();
-    }
-}
-function clickEqual() {
-    var total = 0;
-    var operator = "+";
-    var number;
-    var ops = $("#operation div:odd"); // all operators 
-    var nums = $("#operation div:even"); // all numbers pressed
-    // loop through operators and perform operation
-    for (var i = 0; i < ops.length; i++) {
-        number = Number($(nums[i]).text().trim());
-        total = handleTotal(operators[operator](total, number));
-        console.log(total);
-        operator = $(ops[i]).text().trim();
-    }
-    number = $("#main").text().trim();
-    total = handleTotal(operators[operator](total, number));
-    $("#main").html(`<div class="main-result">${total}</div>`);
-    console.log(total);
-    $("#operation").empty();
-}
 
 $(document).ready(function() {
     $(".buttons button").focus(function (event) {
         $(event.target).css("outline", "none");
     });
 
+    // used unicode strings as keys so as to match html character entities
+    const operators = {
+        "\u002B": function sum(x, y) {
+            return new Big(x).plus(y);
+        },
+        "\u2212": function sub(x, y) {
+            return new Big(x).minus(y);
+        },
+        "\u00D7": function mul(x, y) { 
+            return new Big(x).times(y);
+        },
+        "\u00F7": function div(x, y) {
+            if (y === "0")
+                return "Cannot divide by zero";
+            return new Big(x).div(y);
+        }
+    };
+
+    // handle the total returned by the operator methods
+    // to make it fit display width.
+    let handleTotal = (bigTotal) => {
+        let total; 
+        if (typeof(bigTotal) == "string")
+            total = bigTotal;
+
+        else if (bigTotal.c.length > 12) {
+            if (bigTotal.e > 9) { 
+                // set no. of coefficients(significant digits) to 8
+                // to accomodate exponentiation part on the display.
+                bigTotal.c.length = 8; 
+                total = bigTotal.toString();
+            } else {
+                // if no exponentiation,
+                // set no. of coefficients(significant digits) to 12
+                bigTotal.c.length = 12;     
+                total = bigTotal.toString();    
+            }
+        }
+        else if (bigTotal.e > 12) {
+            total = bigTotal.toExponential();
+        } 
+        else {
+            total = bigTotal.toString();
+        }
+        return total;
+    };
+
+    function clickNumber(e) {
+        var mainDisp = $("#main");
+        var value = $(e.target).val();
+        var numElement = `<div class="main-number">${value}</div>`;
+        
+        // handle number length in the main display
+        if (mainDisp.contents().length >= 12) {
+            // if number length exceeds 12 scale down font-size to
+            // to accumulate upto 16 numbers
+            if (mainDisp.contents().length < 16) {
+                mainDisp.css("font-size", "4.2rem");
+                mainDisp.append(numElement);
+            }
+            else
+                return false;
+        }
+        else if (mainDisp.text() === "Cannot divide by zero")
+            mainDisp.html(numElement);
+        else {
+            // scale up font-size back to original size
+            mainDisp.css("font-size", "5.4rem");
+            // display numbers
+            if ($("#main div:first").text() === "0" && $("#main div").length === 1 && value === "0")
+                return false;
+            else if ($("#main div:first").text() === "0" && value !== "." && $("#main div").length === 1)
+                mainDisp.html(numElement); 
+            else if ($("#main div:first").text() === "0" && value === ".")
+                mainDisp.append(numElement);
+            else if ($("#main div:first").text() === "0" && $($("#main div").get(1)).text() === ".")
+                mainDisp.append(numElement);
+            else if ($("#main div").is(".main-result")) 
+                mainDisp.html(numElement);
+            else
+                mainDisp.append(numElement);
+        }    
+    }
+    function clickClear(e) {
+        $("#operation").empty(); // operations 
+        $("#main").html("<div class='main-number'>0</div>"); // reset main display to 0
+        return false;
+    }
+    function clickBs(e) {
+        var last = $("#main div:last");
+        if ($("#main div").length > 1)
+            last.remove();
+        else  if ($("#main div").length === 1)
+            $("#main").html("<div class='main-number'>0</div>");
+        else
+            return false;
+    }
+    function clickOperator(e) {
+        var operation = $("#operation");
+        var operator, number, operationElem, val;
+        if (!$("#main div").length) // do nothing for empty display
+            return false;
+        else {
+            // extract the operator and number and append to display
+            operator = $(e.target).text();
+            number = $("#main").text();
+            operationElem = `<div class="operation-num"> ${number} </div><div class="operation-op">${operator}</div>`;
+            operation.append(operationElem);
+            $("#main").empty();
+        }
+    }
+    function clickEqual() {
+        var total = 0;
+        var operator = "+";
+        var number;
+        var ops = $("#operation div:odd"); // all operators 
+        var nums = $("#operation div:even"); // all numbers pressed
+        // loop through operators and perform operation on numbers
+        for (var i = 0; i < ops.length; i++) {
+            number = Number($(nums[i]).text().trim());
+            total = handleTotal(operators[operator](total, number));
+            operator = $(ops[i]).text().trim();
+        }
+        number = $("#main").text().trim();
+        total = handleTotal(operators[operator](total, number));
+        if (total == "Cannot divide by zero")
+            $("#main").html(`<div class="main-result-divby0">${total}</div>`);    
+        else
+            $("#main").html(`<div class="main-result">${total}</div>`);
+        $("#operation").empty();
+    }
 
     // clear main display onLoad
     clickClear();
